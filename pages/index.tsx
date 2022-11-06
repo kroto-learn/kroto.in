7a2fe755cf6ad425819504ce30b73f7d6527f99e
Rose Kamal Love <rosekamallove@gmail.com>
2022-11-06 20:30:16 +0530

    initialized: Next App with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+
+  return (
+    <h1 className="text-4xl flex items-center justify-center font-bold h-[100vh]">
+      Welcome to{' '}
+      <a className="underline font-semibold text-blue-400 hover:text-blue-500 cursor-pointer px-1 transition-all">
+        kroto.in
+      </a>
+    </h1>
+  )
+}
