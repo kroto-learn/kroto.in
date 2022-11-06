4634b99e811e28a197eccdffc356db1866666eb5
Rose Kamal Love <rosekamallove@gmail.com>
2022-11-07 00:45:30 +0530

    added: mouse animation

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -1,105 +1,135 @@
-import { IoArrowForwardOutline } from 'react-icons/io5'
+import { IoArrowDown, IoArrowForwardOutline } from 'react-icons/io5'
 import Image from 'next/image'
 import logo from '../../public/kroto-logo.png'
+import { useEffect, useState } from 'react'
 
 export default function Hero() {
+  const [width, setWidth] = useState('50%')
+
+  useEffect(() => {
+    const handleMouseMove = (e: any) => {
+      const width = `${(e.clientX / window.innerWidth) * 100}%`
+      setWidth(width)
+    }
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('touchmove', (e: any) =>
+      handleMouseMove(e.touches[0])
+    )
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [])
+
+  console.log(width)
+
   return (
-    <div className="overflow-hidden bg-split-white-black">
-      <SVGBackground />
-      <div className="flex flex-col items-center justify-center min-h-screen ">
-        <div
-          className={`flex-grow-0  pt-10 transition-opacity drop-shadow-[0_3px_1px_rgba(0,0,0,0.2)]`}
-        >
+    <>
+      <div className="flex bg-[#EF3054] flex-col items-center justify-center min-h-screen w-full absolute">
+        <div className="flex-grow-0 z-10 pt-5 transition-opacity drop-shadow-[0_3px_1px_rgba(0,0,0,0.2)]">
           <Image src={logo} width={512 / 9} height={512 / 9} alt="logo" />
         </div>
-        <div className="flex w-8/12 gap-16 flex-1 flex-col items-center justify-center p-12 text-center font-bold text-white drop-shadow-[0_3px_1px_rgba(0,0,0,0.2)]">
-          <h1 className="text-6xl font-semibold">
-            Are you <span className="text font font-extrabold">struggling</span>{' '}
-            learn to code through video courses
-          </h1>
-          <h3 className="text-4xl font-bold ">I understand you pain.</h3>
-          <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+        <div className="flex z-10 w-8/12 gap-10 flex-1 flex-col items-center justify-center text-center text-white">
+          <div className="drop-shadow-[0_3px_1px_rgba(0,0,0,0.2)]">
+            <h1 className="text-3xl md:text-8xl mb-10 -mt-10">
+              Are you{' '}
+              <span className="text font font-extrabold">struggling</span> to
+              learn to code
+            </h1>
+          </div>
+          <div className="mt-5 sm:mt-0 sm:ml-3 transition-all">
             <a
               href="mailto:kamal@kroto.in"
-              className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-2 text-base font-medium text-[#EF3054] hover:bg-gray-100 md:py-4 md:px-10 md:text-lg transition-all "
+              className="flex flex-col w-full items-center justify-center text-base font-extrabold gap-2 text-white hover:scale-110 active:scale-95 md:py-4 md:px-10 md:text-2xl transition-all"
             >
-              Here&apos;s the Solution
-              <span className="pl-2 transition-all">
-                <IoArrowForwardOutline />
+              <span className="transition-all">Here&apos;s the Solution</span>
+              <span className="transition-all animate-bounce">
+                <IoArrowDown />
               </span>
             </a>
           </div>
         </div>
       </div>
-    </div>
+
+      {/* Extra Background */}
+      <div
+        style={{ width: width }}
+        className="flex bg-[#086788] items-center justify-center min-h-screen absolute "
+      ></div>
+    </>
   )
 }
 
 export const SVGBackground = () => (
-  <div className="relative h-full translate-y-80 max-w-sm md:max-w-lg -z-10 mx-auto">
+  //   <div
+  //     className="hidden overflow-hidden sm:absolute sm:inset-y-0 sm:block sm:h-full sm:w-full"
+  //     aria-hidden="true"
+  //   >
+  <div className="relative mx-auto h-full max-w-7xl">
     <svg
-      className="absolute right-full transform translate-x-1/4 lg:translate-x-1/2"
-      width={404}
-      height={784}
+      className="absolute right-full translate-y-1/4 translate-x-1/4 transform lg:translate-x-1/2"
+      width="404"
+      height="784"
       fill="none"
       viewBox="0 0 404 784"
     >
       <defs>
         <pattern
           id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-          x={0}
-          y={0}
-          width={20}
-          height={20}
+          x="0"
+          y="0"
+          width="20"
+          height="20"
           patternUnits="userSpaceOnUse"
         >
           <rect
-            x={0}
-            y={0}
-            width={4}
-            height={4}
-            className={'text-gray-200'}
+            x="0"
+            y="0"
+            width="4"
+            height="4"
+            className="text-gray-200/20"
             fill="currentColor"
           />
         </pattern>
       </defs>
       <rect
-        width={404}
-        height={784}
+        width="404"
+        height="784"
         fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
       />
     </svg>
     <svg
-      className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
-      width={404}
-      height={784}
+      className="absolute left-full -translate-y-3/4 -translate-x-1/4 transform md:-translate-y-1/2 lg:-translate-x-1/2"
+      width="404"
+      height="784"
       fill="none"
       viewBox="0 0 404 784"
     >
       <defs>
         <pattern
           id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-          x={0}
-          y={0}
-          width={20}
-          height={20}
+          x="0"
+          y="0"
+          width="20"
+          height="20"
           patternUnits="userSpaceOnUse"
         >
           <rect
-            x={0}
-            y={0}
-            width={4}
-            height={4}
-            className={'text-[#282c3440]'}
+            x="0"
+            y="0"
+            width="4"
+            height="4"
+            className="text-gray-200/20"
             fill="currentColor"
           />
         </pattern>
       </defs>
       <rect
-        width={404}
-        height={784}
+        width="404"
+        height="784"
         fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
       />
     </svg>
   </div>
+  //   </div>
 )
