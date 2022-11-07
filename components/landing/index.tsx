707a555aaeacfb6cfca0c11ab73f75f90637896e
Rose Kamal Love <rosekamallove@gmail.com>
2022-11-07 13:47:58 +0530

    finished: hero

--- components/landing/index.tsx
+++ components/landing/index.tsx
@@ -1,9 +1,11 @@
 import Hero from './hero'
+import TutorialHell from './tutorial-hell'
 
 export default function Landing() {
   return (
     <div>
       <Hero />
+      {/* <TutorialHell /> */}
     </div>
   )
 }
