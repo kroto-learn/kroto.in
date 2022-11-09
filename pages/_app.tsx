72dac877e008baeeb1c0ef806c31f6f2ad40da95
Rose Kamal Love <rosekamallove@gmail.com>
2022-11-09 17:52:04 +0530

    added: a lot more og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Kroto</title>
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Kroto - Learn Build Discuss" />
+        <meta property="og:title" content="Kroto - Learn Build Discuss" />
         <meta property="og:url" content="https://www.kroto.in" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="www.kroto.in" />
+        <meta property="twitter:url" content="https://www.kroto.in" />
+        <meta name="twitter:title" content="Kroto - Learn Build Discuss" />
         <meta property="og:image" content="https://www.kroto.in/kroto-og.png" />
         <meta
           property="og:image:url"
@@ -20,7 +25,19 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:image:width" content="630" />
         <meta property="og:image:height" content="1200" />
         <meta
+          name="twitter:image"
+          content="https://www.kroto.in/kroto-og.png"
+        />
+        <meta
           property="og:description"
+          content="Struggling to learn to code? This is the solution."
+        />
+        <meta
+          name="description"
+          content="Struggling to learn to code? This is the solution."
+        />
+        <meta
+          name="twitter:description"
           content="Struggling to learn to code? This is the solution."
         />
       </Head>
