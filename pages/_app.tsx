379266278ed778f2c78e6a68061f2a3d93c9f56c
Rose Kamal Love <rosekamallove@gmail.com>
2022-11-09 17:22:06 +0530

    updated: og for the last fucking time

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ScrollObserver from '../components/scroll-observer'
 import Navbar from '../components/navbar'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ScrollObserver>
-      <Navbar />
-      <Component {...pageProps} />
-    </ScrollObserver>
+    <>
+      <Head>
+        <title>Kroto</title>
+        <meta property="og:title" content="Kroto - Learn Build Discuss" />
+        <meta property="og:image" content="https://kroto.in/kroto-og.png" />
+        <meta
+          property="og:description"
+          content="This is the solution if you're struggling to learn to code by watching videos."
+        />
+      </Head>
+      <ScrollObserver>
+        <Navbar />
+        <Component {...pageProps} />
+      </ScrollObserver>
+    </>
   )
 }
